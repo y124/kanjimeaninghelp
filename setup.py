78aca32c31f9cf6af2c7554228d0cf67a78a13ab
y124 <64955721+y124@users.noreply.github.com>
2021-06-13 21:54:14 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='kanjilearn',  
+     version='0.1',
+     scripts=['kanjilearn'] ,
+     author="y124",
+     author_email="y124@y124.tk",
+     description="Learn kanji with tkinter app",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/y124/kanjimeaninghelp",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
